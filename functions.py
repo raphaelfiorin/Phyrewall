51357630d869a3ff8cf68ff20ae5da2e653c5635
Marcus Castilho <rootmarcus001@gmail.com>
2022-03-01 18:34:30 -0300

    Reformulação de Projeto

---
+++ functions.py
@@ -0,0 +1,98 @@
+from time import sleep
+import paramiko,os
+
+class service():
+    def cls():
+        os.system('cd C:\\Windows|cls' if os.name == 'nt' else 'clear')
+
+    #banner
+    def banner():
+        print("""
+        ██████╗ ██╗  ██╗██╗   ██╗██████╗ ███████╗██╗    ██╗ █████╗ ██╗     ██╗
+        ██╔══██╗██║  ██║╚██╗ ██╔╝██╔══██╗██╔════╝██║    ██║██╔══██╗██║     ██║
+        ██████╔╝███████║ ╚████╔╝ ██████╔╝█████╗  ██║ █╗ ██║███████║██║     ██║
+        ██╔═══╝ ██╔══██║  ╚██╔╝  ██╔══██╗██╔══╝  ██║███╗██║██╔══██║██║     ██║
+        ██║     ██║  ██║   ██║   ██║  ██║███████╗╚███╔███╔╝██║  ██║███████╗███████╗
+        ╚═╝     ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚══════╝ ╚══╝╚══╝ ╚═╝  ╚═╝╚══════╝╚══════╝ \033[1m\033[1;31m0.1\033[0;0m
+                                                    \033[1m\033[32mby: Raphael Fiorin & Marcus Castilho\033[0;0m""")
+
+class fortigateFunctions():
+    #Aqui está instanciado TODAS as funções pertencentes ao Fortigate(FireWall)
+    
+    def create_user_ssh():
+        print('\n\033[32m[SSH]\033[0;0m - Estabelecendo conexão SSH ')
+
+        while True:
+            try:
+                host = input('└> IP do Host: ')
+                port = int(input('└> Porta de acesso: '))
+                user = input('└> Usuário de acesso: ')
+                password = input('└> Senha de acesso: ')
+                print('\033[32mConectando via SSH...\033[m')
+                
+                ssh = paramiko.SSHClient()
+                ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            
+                ssh.connect(host, port, user, password)
+                print('\n\033[34m[SSH Conectado]\033[m')
+                sleep(1)
+                break
+            except ValueError as error:
+                print('\033[33mValor inválido!\033[m Tente novamente...\n {}'.format(error))
+                sleep(2)
+                continue
+
+            except paramiko.AuthenticationException as error:
+                print("\033[31mErro de login: \033[0;0m" + str(err))
+                sleep(1)
+                print('Tente novente...\n {}'.format(error))
+                sleep(1)
+                continue
+        
+        while True:
+            print('\n\033[32mCriação de Usuário\033[0;0m')
+            try:
+                nome = input('└> Escolha um nome de usuário: ')
+                senha = input('└> Digite a senha: ')
+                usertype = int(input("""
+        \033[36mTipo de usuário\033[0;0m
+        \033[32m[1]\033[0;0m - Admin_no_access
+        \033[32m[2]\033[0;0m - Prof_admin
+        \033[32m[3]\033[0;0m - Super_admin
+        \033[32m[4]\033[0;0m - Super_admin_readonly
+        \033[31m[5]\033[0;0m - Voltar ao Menu
+            └> """))
+            except ValueError:
+                print('\033[33mValor inválido!\033[m Tente novamente...\n {}'.format(error))
+
+            if usertype == 1:
+                usertype = 'admin_no_acess'
+                break
+            elif usertype == 2:
+                usertype = 'prof_admin'
+                break
+            elif usertype == 3:
+                usertype = 'super_admin'
+                break
+            elif usertype == 4:
+                usertype = 'super_admin_readonly'
+                break
+            elif usertype == 5:
+                print('Voltando ao menu!')
+            else:
+                print(' \033[31mEscolha os itens do menu!\033[0;0m')
+                continue
+
+        stdin, stdout, stderr = ssh.exec_command("config system admin \n" + "edit {} \n".format(nome) + " set accprofile {} \n".format(usertype) + "set vdom root \n" + "set password {} \n".format(senha) + "end \n")
+        # lines = stdout.readlines()
+        # print(lines)
+        print('\n\033[34mUsuario criado com sucesso\033[m')
+        print(f'''
+        └> User: {nome}
+        └> Tipo de user: {usertype}''')
+        sleep(3)
+
+
+#[17:08, 01/03/2022] Gordin: 170.81.131.237
+#[17:08, 01/03/2022] Gordin: python
+#R89344697
--- main.py
+++ main.py
@@ -1,14 +1,34 @@
-from lib import libAll
-from Fortigate import libFg
+from time import sleep
+import functions
 
-libAll.cls()
-libAll.banner()
-menu = int(input('''
-\033[32m[1]\033[0;0m - Fortigate - Tarefas Automatizadas
-\033[31m[2]\033[0;0m - Palo alto - Cooming soon...
-└> '''))
 
-if menu == 1:
-    libFg.connection()
-elif menu == 2:
-    pass
+while True:
+    functions.service.cls()
+    functions.service.banner()
+    try:
+        sleep(1)
+        print('\n\033[34mEscolha o tipo de FireWall\033[m')
+        sleep(2)
+        op = int(input('''
+[01] Fortigate
+\033[31m[02] Sair\033[m
+ └> '''))
+
+        if op == 1 or op == str(op):
+            print('\n\033[34mEscolha o tipo de conexão\033[m')
+            sleep(1)
+        typeConnection = int(input('''
+    [01] SSH
+    [02] Cabo Console
+    └> '''))
+        if typeConnection == 1 or typeConnection == str(typeConnection):
+            functions.fortigateFunctions.create_user_ssh()
+        elif typeConnection == 2 or typeConnection == str(typeConnection):
+            print('\033[31mEm desenvolvimento...\033[m')
+            sleep(2)
+            continue
+        
+    except ValueError:
+        print('\033[33mValor inválido!\033[m Tente novamente...\n {}'.format(error))
+        sleep(2)
+        continue
