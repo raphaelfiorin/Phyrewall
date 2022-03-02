e6c927279544df7a4e8625e28b0c4d1728c2cdd3
Marcus Castilho <rootmarcus001@gmail.com>
2022-03-02 20:29:46 -0300

    Cabo console add

--- functions.py
+++ functions.py
@@ -34,9 +34,7 @@ class service():
                         if op == 1:
                             fortigateFunctions.create_user_ssh()
                         elif op == 2:
-                            print('\033[31mEm desenvolvimento\033[m')
-                            sleep(1)
-                            continue
+                            fortigateFunctions.create_user_console()
                     except ValueError as error:
                         print('\033[33mValor inválido!\033[m Tente novamente...\n {}'.format(error))
                         sleep(2)
@@ -141,3 +139,47 @@ class fortigateFunctions():
         └> User: {nome}
         └> Tipo de user: {usertype}''')
         sleep(3)
+
+    def create_user_console():
+        while True:
+            print('\n\033[32m[CABO CONSOLE]\033[0;0m - Criação de user')
+            try:
+                portConsole = input('└> Número da porta: ')
+                user = input('└> Digite o user: ')
+                password = input('└> Digite a senha: ')
+                # criando o usuario admin
+                nome = input('└> Digite o nome do user: ')
+                senha = input('└> Digite a senha: ')
+                usertype = int(input("""
+        \033[32m[CABO CONSOLE]\033[0;0m - Tipo de user:
+        \033[32m[1]\033[0;0m - Admin_no_access
+        \033[32m[2]\033[0;0m - Prof_admin
+        \033[32m[3]\033[0;0m - Super_admin
+        \033[32m[4]\033[0;0m - Super_admin_readonly
+        \033[31m[5]\033[0;0m - Voltar ao Menu
+        └> """))
+                if usertype == 1:
+                    usertype = 'admin_no_acess'
+                elif usertype == 2:
+                    usertype = 'prof_admin'
+                elif usertype == 3:
+                    usertype = 'super_admin'
+                elif usertype == 4:
+                    usertype = 'super_admin_readonly'
+                elif usertype == 5:
+                    print('Voltando ao menu!')
+                else:
+                    print(' \033[31mEscolha os itens do menu!\033[0;0m')
+
+                ser = serial.Serial(portConsole, 9600)
+                ser.write(str.encode('{}\n'.format(user) + '{}\n'.format(password) + "config system admin \n" + "edit {} \n".format(nome) + " set accprofile {} \n".format(usertype) + "set vdom root \n" + "set password {} \n".format(senha) + "end \n"))
+                # resposta = ser.read(99999).decode('utf-8')
+                print('\n\033[32m[CABO CONSOLE]\033[0;0m - Usuario criado com sucesso')
+                print(f'''└> User: {nome}
+                └> Tipo de user: {usertype}''')
+                sleep(3)
+
+            except ValueError as error:
+                print('\033[33mValor inválido!\033[m Tente novamente...\n {}'.format(error))
+                sleep(2)
+                continue
